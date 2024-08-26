898b901b1f85d3a3f60b90d84798be2654d8115e
Indoraptor0902 <romitsachdev@gmail.com>
2024-08-25 23:01:24 -0700

    Using base html templates

--- health_app/health_app/settings.py
+++ health_app/health_app/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'health_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
