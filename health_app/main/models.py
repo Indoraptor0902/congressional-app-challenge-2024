7e5bcc5e04748d91ef5e88df8406a40521e74c20
Indoraptor0902 <romitsachdev@gmail.com>
2024-08-30 22:10:11 -0700

    Created user saving to database

--- health_app/main/models.py
+++ health_app/main/models.py
@@ -1,75 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
-class UserInfo(models.Model):
-    username = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
+class HealthInformation(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='health_info')
+    age = models.PositiveIntegerField(blank=True, default=0)
+    bmi = models.FloatField(blank=True, default=0)
+    insulin = models.FloatField(blank=True, default=0)
+    glucose = models.FloatField(blank=True, default=0)
+    blood_pressure = models.FloatField(blank=True, default=0)
 
     def __str__(self):
-        return self.username
+        return f"{self.user.username}'s Health Information"
 
 
-class Pregnancies(models.Model):
-    userinfo = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
-    pregnancies = models.IntegerField()
-
-    def __str__(self):
-        return str(self.pregnancies)
-
-class Glucose(models.Model):
-    userinfo = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
-    glucose = models.IntegerField()
-
-    def __str__(self):
-        return str(self.glucose)
-
-class BloodPressure(models.Model):
-    userinfo = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
-    bloodpressure = models.IntegerField()
-
-    def __str__(self):
-        return str(self.bloodpressure)
-
-class SkinThickness(models.Model):
-    userinfo = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
-    skinthickness = models.IntegerField()
-
-    def __str__(self):
-        return str(self.skinthickness)
-
-class Insulin(models.Model):
-    userinfo = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
-    insulin = models.IntegerField()
-
-    def __str__(self):
-        return str(self.insulin)
-
-class Height(models.Model):
-    userinfo = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
-    height = models.IntegerField()
-
-    def __str__(self):
-        return str(self.height)
-
-class Weight(models.Model):
-    userinfo = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
-    weight = models.IntegerField()
-
-    def __str__(self):
-        return str(self.weight)
-
-class DiabetesPedigreeFunction(models.Model):
-    userinfo = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
-    diabetespedigreefunction = models.FloatField()
-
-    def __str__(self):
-        return str(self.diabetespedigreefunction)
-
-class Age(models.Model):
-    userinfo = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
-    age = models.IntegerField()
-
-    def __str__(self):
-        return str(self.age)
--- health_app/main/views.py
+++ health_app/main/views.py
@@ -1,4 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import *
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+def update_health_info(request):
+    user, created = User.objects.get_or_create(username='testuser')
+    # Retrieve the existing health information for the user
+    health_info, created = HealthInformation.objects.get_or_create(user=user)
+
+    if request.method == "POST":
+        # Update the health information with the data from the form
+        health_info.age = request.POST.get('age')
+        health_info.bmi = request.POST.get('bmi')
+        health_info.insulin = request.POST.get('insulin')
+        health_info.glucose = request.POST.get('glucose')
+        health_info.blood_pressure = request.POST.get('blood_pressure')
+        # Add any additional fields here
+
+        health_info.save()
+
+        # Redirect to a confirmation page or back to the form with a success message
+        return redirect('health_info_success')
+
+    return render(request, 'update_health_info.html', {
+        'health_info': health_info
+    })
+
+def health_info_success(request):
+    return render(request, 'health_info_success.html')
