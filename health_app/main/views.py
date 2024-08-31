6a89c7a9b0cc02e1be89ce5c239ee6f7566b1fde
Indoraptor0902 <romitsachdev@gmail.com>
2024-08-30 22:24:21 -0700

    updating styling

--- health_app/main/views.py
+++ health_app/main/views.py
@@ -6,21 +6,17 @@ from django.contrib.auth.decorators import login_required
 
 def update_health_info(request):
     user, created = User.objects.get_or_create(username='testuser')
-    # Retrieve the existing health information for the user
     health_info, created = HealthInformation.objects.get_or_create(user=user)
 
     if request.method == "POST":
-        # Update the health information with the data from the form
         health_info.age = request.POST.get('age')
         health_info.bmi = request.POST.get('bmi')
         health_info.insulin = request.POST.get('insulin')
         health_info.glucose = request.POST.get('glucose')
         health_info.blood_pressure = request.POST.get('blood_pressure')
-        # Add any additional fields here
 
         health_info.save()
 
-        # Redirect to a confirmation page or back to the form with a success message
         return redirect('health_info_success')
 
     return render(request, 'update_health_info.html', {
